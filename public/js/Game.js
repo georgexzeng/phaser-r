f5fe748db90ed16f7e0f792e7e904a682d2a85d2
George Zeng <george.x.zeng@gmail.com>
2014-09-10 12:18:36 -0400

    updated firebase code

--- public/js/Game.js
+++ public/js/Game.js
@@ -13,6 +13,99 @@ Game = function(game) {
   var fishSfx;
 };
 
+// ------------ FIREBASE ------------
+
+var firebase = {};
+var fb = {};
+
+
+firebase.firebaseSetup = function(roomSession) {
+  this.ref = new Firebase("https://polarbearpanic.firebaseio.com/");
+  this.room = this.ref.child(roomSession);
+  this.chat = this.room.child("chat");
+  this.game = this.room.child("game");
+  this.pause = this.game.child("pause");
+  this.player1 = this.game.child("player1");
+  this.player2 = this.game.child("player2");
+  this.person = this.game.child("person");
+  this.ghost = this.game.child("ghost");
+}
+
+// firebase.recieveMessage = function(snapshot) {
+//   var message = snapshot.val().message;
+//   var name = message.name;
+//   var content = message.content;
+//   var output = "<div class='message'><b>" + name + ":</b> ";
+//         output += "<span>" + content + "</span>";
+//         output += "</div>";
+//   $("#msg-output").append(output);
+// }
+
+// firebase.preMessage = function(event) {
+//   event.preventDefault();
+//   var name = $("#user-name").val()
+//   var content = $("#msg-input").val()
+//   $("#msg-input").val("");
+//   firebase.sendMessage(fb, name, content);
+// }
+
+// firebase.sendMessage = function(firebase, name, content) {
+//   firebase.chat.push({
+//     message : {
+//       name: name,
+//       content: content,
+//       timestamp: Firebase.ServerValue.TIMESTAMP
+//     }
+//   });
+// }
+
+firebase.sendCoordinates = function(sprite) {
+  firebase.game.push({
+    message : {
+      sprite: sprite,
+      x_coordinates: sprite.x,
+      y_coordinates: sprite.y,
+      timestamp: Firebase.ServerValue.TIMESTAMP
+    }
+  });
+}
+
+// ------------ FIREBASE ------------
+
+
+$(document).ready(function() {
+  roomSession = $("#room-session").val();
+  if (typeof roomSession !== "undefined") {
+    fb = new firebase.firebaseSetup(roomSession);
+  }
+
+  document.onkeydown = function (e) {
+    if(e.which == 80) {
+      fb.pause.set(true);
+    } else if (e.which == 82) {
+      fb.pause.set(false);
+    }
+  }
+
+  fb.pause.on("value", function(snapshot) {
+    game.paused = snapshot.val();
+  });
+
+  fb.player1.on("value", function(snapshot) {
+    if ((snapshot.val() === true) && (currentPlayer !== "player1")) {
+      fb.player2.on("value", function(snapshot) {
+        if (snapshot.val() !== true) {
+          modals.confirmP2();
+        }
+      });
+    } else {
+      modals.confirmP1(roomSession);    
+    }
+  });
+  
+});
+
+
 var Lake = function(game, x, y, width, height) {
   Phaser.TileSprite.call(this, game, x, y, width, height, 'lakes');
   this.autoScroll(-200,0);
